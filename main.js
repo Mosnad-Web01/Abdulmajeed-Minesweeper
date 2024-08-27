99fd107e244ee0819fd9178cc1c3af955e0b038c
Abdulmajeed Alselmi <hh738hhh@gmail.com>
2024-08-27 14:39:23 +0300

    add function that takes an array representation of a Minesweeper board

--- main.js
+++ main.js
@@ -1,3 +1,36 @@
+function mineSweeper(src) {
+    const rows = src.length;
+    const cols = src[0].length;
+    const output = Array.from({ length: rows }, () => Array(cols).fill(0));
+
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < cols; j++) {
+            if (src[i][j] === 1) {
+                output[i][j] = 9;
+                continue;
+            }
+            let mineCount = 0;
+            for (let x = -1; x <= 1; x++) {
+                for (let y = -1; y <= 1; y++) {
+                    const newRow = i + x;
+                    const newCol = j + y;
+                    if (
+                        newRow >= 0 &&
+                        newRow < rows &&
+                        newCol >= 0 &&
+                        newCol < cols &&
+                        src[newRow][newCol] === 1
+                    ) {
+                        mineCount++;
+                    }
+                }
+            }
+            output[i][j] = mineCount;
+        }
+    }
+    return output;
+}
+
 const input = [
   [0, 1, 0, 0],
   [0, 0, 1, 0],
@@ -5,15 +38,5 @@ const input = [
   [1, 1, 0, 0],
 ];
 
-const output = [
-  [1, 9, 2, 1],
-  [2, 3, 9, 2],
-  [3, 9, 4, 9],
-  [9, 9, 3, 1],
-]
-
-function mainSweeper(src){
- return;
-}
-
-mineSweeper(input)
+const output = mineSweeper(input);
+console.log(output);
